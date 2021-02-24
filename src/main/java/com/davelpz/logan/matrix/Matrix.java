3627ef8ebdc1a9975b560d2f4cac87dc60112311
David Lopez <dlopez@pointandpay.com>
2021-02-24 13:31:00 -0500

    wip matrixes

--- src/main/java/com/davelpz/logan/matrix/Matrix.java
+++ src/main/java/com/davelpz/logan/matrix/Matrix.java
@@ -1,14 +1,45 @@
 package com.davelpz.logan.matrix;
 
+import com.davelpz.logan.tuple.Tuple;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class Matrix {
     private int dimension;
     private double[][] data;
 
-    public  Matrix(int dimension) {
+    public Matrix(int dimension) {
         this.dimension = dimension;
         this.data = new double[dimension][dimension];
+    }
+
+    public static Matrix multiply(Matrix a, Matrix b) {
+        Matrix m = new Matrix(b.dimension);
+        double[][] dataA = a.data;
+        double[][] dataB = b.data;
+
+        for (int row = 0; row < dataA.length; row++) {
+            for (int col = 0; col < dataB.length; col++) {
+                m.data[row][col] =
+                        dataA[row][0] * dataB[0][col] + dataA[row][1] * dataB[1][col] + dataA[row][2] * dataB[2][col] + dataA[row][3] * dataB[3][col];
+            }
+        }
+
+        return m;
+    }
+
+    public static Tuple multiply(Matrix a, Tuple b) {
+        Tuple t = new Tuple(0,0,0,0);
+        double[][] dataA = a.data;
+
+        t.setX(dataA[0][0] * b.x() + dataA[0][1] * b.y() + dataA[0][2] * b.z() + dataA[0][3] * b.w());
+        t.setY(dataA[1][0] * b.x() + dataA[1][1] * b.y() + dataA[1][2] * b.z() + dataA[1][3] * b.w());
+        t.setZ(dataA[2][0] * b.x() + dataA[2][1] * b.y() + dataA[2][2] * b.z() + dataA[2][3] * b.w());
+        t.setW(dataA[3][0] * b.x() + dataA[3][1] * b.y() + dataA[3][2] * b.z() + dataA[3][3] * b.w());
+
+        return t;
     }
 
     public boolean setData(List<List<Double>> listdata) {
@@ -16,17 +47,52 @@ public class Matrix {
             return false;
         }
 
-        for (int row=0;row<listdata.size();row++) {
+        for (int row = 0; row < listdata.size(); row++) {
             List<Double> columns = listdata.get(row);
             if (columns.size() != dimension) {
                 return false;
             }
-            for (int col=0;col<columns.size();col++) {
+            for (int col = 0; col < columns.size(); col++) {
                 this.data[row][col] = columns.get(col);
             }
         }
 
         return true;
+    }
+
+    public boolean equals(Matrix b) {
+        if (this.dimension != b.dimension) {
+            return false;
+        }
+
+        double[][] aData = this.data;
+        double[][] bData = b.data;
+
+        for (int row = 0; row < dimension; row++) {
+            for (int col = 0; col < dimension; col++) {
+                if (Math.abs(aData[row][col] - bData[row][col]) > Tuple.EPSILON ){
+                    return false;
+                }
+            }
+        }
+
+        //return Arrays.deepEquals(this.data, b.data);
+        return true;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Matrix matrix = (Matrix) o;
+        return dimension == matrix.dimension && Arrays.deepEquals(data, matrix.data);
+    }
+
+    @Override public int hashCode() {
+        int result = Objects.hash(dimension);
+        result = 31 * result + Arrays.hashCode(data);
+        return result;
     }
 
     public double get(int row, int col) {
--- src/test/java/com/davelpz/logan/matrix/StepDefinitions.java
+++ src/test/java/com/davelpz/logan/matrix/StepDefinitions.java
@@ -2,6 +2,7 @@ package com.davelpz.logan.matrix;
 
 import com.davelpz.logan.canvas.Canvas;
 import com.davelpz.logan.color.Color;
+import com.davelpz.logan.tuple.Tuple;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -9,11 +10,10 @@ import io.cucumber.java.en.When;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class StepDefinitions {
-    Matrix m;
+    Matrix a,b,m;
 
     @Given("the following 4x4 matrix M:")
     public void the_following_4x4_matrix_m(io.cucumber.datatable.DataTable dataTable) {
@@ -33,6 +33,10 @@ public class StepDefinitions {
     public void m00(Double double1) {
         assertEquals(double1, m.get(0,0), 0.00001);
     }
+    @Then("M[0,1] = {double}")
+    public void m01(Double double1) {
+        assertEquals(double1, m.get(0,1), 0.00001);
+    }
     @Then("M[0,3] = {double}")
     public void m03(Double double1) {
         assertEquals(double1, m.get(0,3), 0.00001);
@@ -40,6 +44,10 @@ public class StepDefinitions {
     @Then("M[1,0] = {double}")
     public void m10(Double double1) {
         assertEquals(double1, m.get(1,0), 0.00001);
+    }
+    @Then("M[1,1] = {double}")
+    public void m11(Double double1) {
+        assertEquals(double1, m.get(1,1), 0.00001);
     }
     @Then("M[1,2] = {double}")
     public void m12(Double double1) {
@@ -56,6 +64,104 @@ public class StepDefinitions {
     @Then("M[3,2] = {double}")
     public void m32(Double double1) {
         assertEquals(double1, m.get(3,2), 0.00001);
+    }
+
+    @Given("the following 2x2 matrix M:")
+    public void the_following_2x2_matrix_m(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        List<List<Double>> data = dataTable.asLists(Double.TYPE);
+        m = new Matrix(2);
+        m.setData(data);
+    }
+
+    @Given("the following 3x3 matrix M:")
+    public void the_following_3x3_matrix_m(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        List<List<Double>> data = dataTable.asLists(Double.TYPE);
+        m = new Matrix(3);
+        m.setData(data);
+    }
+
+    @Given("the following matrix A:")
+    public void the_following_matrix_a(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        List<List<Double>> data = dataTable.asLists(Double.TYPE);
+        a = new Matrix(4);
+        a.setData(data);
+    }
+
+    @Given("the following matrix B:")
+    public void the_following_matrix_b(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        List<List<Double>> data = dataTable.asLists(Double.TYPE);
+        b = new Matrix(4);
+        b.setData(data);
+    }
+
+    @Then("A equals B")
+    public void a_equals_b() {
+        assertTrue(a.equals(b));
+    }
+
+    @Then("A not equals B")
+    public void a_not_equals_b() {
+        assertFalse(a.equals(b));
+    }
+
+    @Then("A mul B is the following 4x4 matrix:")
+    public void a_mul_b_is_the_following_4x4_matrix(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        List<List<Double>> data = dataTable.asLists(Double.TYPE);
+        Matrix expectedResult = new Matrix(4);
+        expectedResult.setData(data);
+
+        Matrix result = Matrix.multiply(a,b);
+
+        assertTrue(result.equals(expectedResult));
+    }
+
+    Tuple tb;
+
+    @Given("tb ← tuple\\({int}, {int}, {int}, {int})")
+    public void tb_tuple(Integer int1, Integer int2, Integer int3, Integer int4) {
+        tb = new Tuple(int1,int2,int3,int4);
+    }
+
+    @Then("A mul tb = tuple\\({int}, {int}, {int}, {int})")
+    public void a_mul_tb_tuple(Integer int1, Integer int2, Integer int3, Integer int4) {
+        Tuple t = new Tuple(int1,int2,int3,int4);
+        Tuple result = Matrix.multiply(a,tb);
+        assertTrue(t.equals(result));
     }
 
 }
