9747c9c53abbe9b99ab849c2b0881f8c95ccbf60
davelpz <davelpz@gmail.com>
2021-03-07 20:36:48 -0500

    chapter 9 done

--- src/test/java/com/davelpz/logan/RendererTest.java
+++ src/test/java/com/davelpz/logan/RendererTest.java
@@ -6,6 +6,7 @@ import com.davelpz.logan.color.Color;
 import com.davelpz.logan.light.PointLight;
 import com.davelpz.logan.material.Material;
 import com.davelpz.logan.matrix.Matrix;
+import com.davelpz.logan.shapes.Plane;
 import com.davelpz.logan.shapes.Sphere;
 import com.davelpz.logan.tuple.Tuple;
 import com.davelpz.logan.world.World;
@@ -88,6 +89,67 @@ public class RendererTest {
         world.getObjects().add(left);
 
         Camera c = new Camera(100,50,Math.PI/3.0);
+        c.setTransform(World.view_transform(Tuple.point(0,1.5,-5),
+                Tuple.point(0,1,0),
+                Tuple.vector(0,1,0)));
+
+        Canvas canvas = Renderer.render(c,world);
+        canvas.canvasToPPMFile("output.ppm");
+    }
+
+    // chapter 9
+    @Test
+    public void testRender3() throws IOException {
+        Plane floor = new Plane();
+        //floor.setTransform(Matrix.scaling(10,0.01,10));
+        //floor.setTransform(Matrix.translation(0,0.0,0));
+        //floor.setTransform(Matrix.rotationX(-Math.PI/16.0));
+        floor.setMaterial(new Material());
+        floor.getMaterial().getColor().set(1,0.9,0.9);
+        floor.getMaterial().setSpecular(0);
+
+        Plane wall = new Plane();
+        wall.setTransform(Matrix.translation(0,0,3).multiply(Matrix.rotationX(-Math.PI/2)));
+        wall.setMaterial(new Material());
+        wall.getMaterial().getColor().set(1,0.9,0.9);
+        wall.getMaterial().setSpecular(0);
+
+        double specular = 0.3;
+        double diffuse = 0.7;
+
+        Sphere middle = new Sphere();
+        middle.setTransform(Matrix.translation(-0.5,1,0.5));
+        middle.setMaterial(new Material());
+        middle.getMaterial().setColor(new Color(0.1,1,0.5));
+        middle.getMaterial().setDiffuse(diffuse);
+        middle.getMaterial().setSpecular(specular);
+
+        Sphere right = new Sphere();
+        right.setTransform(Matrix.translation(1.5,0.5,-0.5)
+                .multiply(Matrix.scaling(0.5,0.5,0.5)));
+        right.setMaterial(new Material());
+        right.getMaterial().setColor(new Color(0.5,1,0.1));
+        right.getMaterial().setDiffuse(diffuse);
+        right.getMaterial().setSpecular(specular);
+
+        Sphere left = new Sphere();
+        left.setTransform(Matrix.translation(-1.5,0.33,-0.75)
+                .multiply(Matrix.scaling(0.33,0.33,0.33)));
+        left.setMaterial(new Material());
+        left.getMaterial().setColor(new Color(1,0.8,0.1));
+        left.getMaterial().setDiffuse(diffuse);
+        left.getMaterial().setSpecular(specular);
+
+        World world = new World();
+        world.getLights().add(new PointLight(new Color(1,1,1), Tuple.point(-10,10,-10)));
+
+        world.getObjects().add(floor);
+        world.getObjects().add(wall);
+        world.getObjects().add(middle);
+        world.getObjects().add(right);
+        world.getObjects().add(left);
+
+        Camera c = new Camera(400,200,Math.PI/3.0);
         c.setTransform(World.view_transform(Tuple.point(0,1.5,-5),
                 Tuple.point(0,1,0),
                 Tuple.vector(0,1,0)));
