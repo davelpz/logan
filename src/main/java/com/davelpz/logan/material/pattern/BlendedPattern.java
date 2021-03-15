8add50a2556890e091018b6ddb6e5273f0501dcb
David Lopez <dlopez@pointandpay.com>
2021-03-15 16:33:54 -0400

    BlendedPattern added

--- src/main/java/com/davelpz/logan/material/pattern/BlendedPattern.java
+++ src/main/java/com/davelpz/logan/material/pattern/BlendedPattern.java
@@ -13,14 +13,6 @@ public class BlendedPattern extends AbstractPattern {
         this.b = b;
     }
 
-    /*
-    @Override public Color pattern_at_shape(Shape object, Tuple world_point) {
-        Color colorA = a.pattern_at_shape(object,world_point);
-        Color colorB = b.pattern_at_shape(object,world_point);
-        Color blendedColor = colorA.add(colorB).div(2.0);
-        return blendedColor;
-    }*/
-
     @Override public Color pattern_at(Tuple point) {
         Color colorA = a.pattern_at_object_point(point);
         Color colorB = b.pattern_at_object_point(point);
